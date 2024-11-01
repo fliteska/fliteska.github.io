fcf6dd7c0d877c72514fb5e80aa8a5d1d08065f3
Fliteska <fliteska@gmail.com>
2024-11-01 11:43:03 +0000

    Restructure content

--- src/data.js
+++ src/data.js
@@ -5,14 +5,13 @@ export const navUrls = [
 ];
 
 export const loadOrders = [
-  { title: "Rebuilding Far Harbor", href: "#" },
-  { title: "Lore-friendly Settlement Building", href: "#" },
-  { title: "Extreme Settlement Building", href: "#" },
+  { title: "Rebuilding Far Harbor", href: "/load-orders/rebuilding-far-harbor" },
+  { title: "Lore-friendly Settlement Building", href: "/load-orders/lore-friendly-settlement-building" },
 ];
 
 export const playlists = [
   { title: "Rebuilding Far Harbor: Echo Lake Lumber", href: "", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, enim?" },
-  { title: "Lore-friendly Settlement Building: Oberland Station", href: "", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, enim?" },
-  { title: "Lore-friendly Settlement Building: Outpost Zimonja", href: "", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, enim?" },
-  { title: "Lore-friendly Settlement Building: Tenpines Bluff", href: "", description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, enim?" },
+  { title: "Lore-friendly Settlement Building: Oberland Station", href: "https://www.youtube.com/playlist?list=PLIF9YwjqqBUve0MCoU5l3YMVjOlZ4z2aP", description: "A train yard settlement for travelling wastelanders." },
+  { title: "Lore-friendly Settlement Building: Outpost Zimonja", href: "https://www.youtube.com/playlist?list=PLIF9YwjqqBUu6-KxBIaq0vcZRzL-ibKpV", description: "A small military outpost controlled by The Minutemen." },
+  { title: "Lore-friendly Settlement Building: Tenpines Bluff", href: "https://www.youtube.com/playlist?list=PLIF9YwjqqBUuVUS90nPqzaj8vQIEJAHTq", description: "A tight-knit farming community in the north of The Commonwealth." },
 ];
