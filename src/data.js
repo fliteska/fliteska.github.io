46b6c4b7d86749b0a38750040a5f13e7202a8763
Fliteska <fliteska@gmail.com>
2024-11-23 20:07:20 +0000

    chore: Add spectacle island playlist

--- src/data.js
+++ src/data.js
@@ -11,7 +11,8 @@ export const loadOrders = [
 ];
 
 export const playlists = [
-  { title: "Rebuilding Far Harbor: Echo Lake Lumber", href: "https://www.youtube.com/playlist?list=PLIF9YwjqqBUv01plZB0HziwIPB3IRhuEl", description: "A small community inspired by TWD's Hilltop Colony" },
+  { title: "Spectacle Island", href: "https://www.youtube.com/playlist?list=PLIF9YwjqqBUunKeSc3kP0ppn2LmjoleB5", description: "A lore-friendly Spectacle Island settlement build." },
+  { title: "Rebuilding Far Harbor: Echo Lake Lumber", href: "https://www.youtube.com/playlist?list=PLIF9YwjqqBUv01plZB0HziwIPB3IRhuEl", description: "A small community inspired by TWD's Hilltop Colony." },
   { title: "Lore-friendly Settlement Building: Oberland Station", href: "https://www.youtube.com/playlist?list=PLIF9YwjqqBUve0MCoU5l3YMVjOlZ4z2aP", description: "A train yard settlement for travelling wastelanders." },
   { title: "Lore-friendly Settlement Building: Outpost Zimonja", href: "https://www.youtube.com/playlist?list=PLIF9YwjqqBUu6-KxBIaq0vcZRzL-ibKpV", description: "A small military outpost controlled by The Minutemen." },
   { title: "Lore-friendly Settlement Building: Tenpines Bluff", href: "https://www.youtube.com/playlist?list=PLIF9YwjqqBUuVUS90nPqzaj8vQIEJAHTq", description: "A tight-knit farming community in the north of The Commonwealth." },
