013f1da9b035889f1c5701e16dac0d2c15fed021
Fliteska <fliteska@gmail.com>
2026-01-06 09:26:50 +0000

    chore: add new modlist

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://fliteska.github.io',
-    integrations: [
-        tailwind(),
-        mdx(),
-    ],
+  site: 'https://fliteska.github.io',
+
+  integrations: [
+      mdx(),
+  ],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
